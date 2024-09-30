4a3605283a6b05e9f0190c13a43d8eb0dcc0503f
“Roni <roni.rusmayadi@global-infotech.co.id>
2024-09-30 10:57:25 +0700

    update

---
+++ test/data/datasource/movie_remote_data_source.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/mockito.dart';
+import 'package:movie_app/data/datasources/movie_remote_data_source.dart';
+import 'package:movie_app/data/models/movie_model.dart';
+import 'package:movie_app/utils/exception.dart';
+
+import '../../helpers/test_helper.mocks.dart';
+import '../../json_reader.dart';
+
+void main() {
+  const baseUrl =
+      'https://api.themoviedb.org/3/discover/movie?include_adult=false';
+
+  late MovieRemoteDataSourceImpl dataSource;
+  late MockHttpClient mockHttpClient;
+
+  setUp(() {
+    mockHttpClient = MockHttpClient();
+    dataSource = MovieRemoteDataSourceImpl(client: mockHttpClient);
+  });
+
+  group('get Movies', () {
+    final tMovieList = (readJson('dummy_data/Movie.json') as List)
+        .map((x) => MovieModel.fromJson(json.decode(x)))
+        .toList();
+
+    test('should return list of Movie Model when the response code is 200',
+        () async {
+      // arrange
+      when(mockHttpClient.get(Uri.parse('$baseUrl/Movie'))).thenAnswer(
+          (_) async => http.Response(readJson('dummy_data/movie.json'), 200));
+      // act
+      final result = await dataSource.getMovie();
+      // assert
+      expect(result, equals(tMovieList));
+    });
+
+    test(
+        'should throw a ServerException when the response code is 404 or other',
+        () async {
+      // arrange
+      when(mockHttpClient.get(Uri.parse('$baseUrl/Movie')))
+          .thenAnswer((_) async => http.Response('Not Found', 404));
+      // act
+      final call = dataSource.getMovie();
+      // assert
+      expect(() => call, throwsA(isA<ServerException>()));
+    });
+  });
+}
